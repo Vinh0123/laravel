c00586d20ee2b89a6953569d1e79b93f90c632c1
Vinh0123 <vinhtranvan314@gmail.com>
2025-03-04 08:34:53 +0700

    database

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\finderController;
 use App\Http\Controllers\objectal;
+use App\Http\Controllers\createTableController;
+
 Route::get('/', [Objectal::class,'index']);
 Route::get('/AddProductal', [objectal::class,'create']);
 Route::get('/Detail/{id}', [App\Http\Controllers\objectal::class,'show']);
 Route::get('/UpdateProduct/{id}', [App\Http\Controllers\objectal::class,'edit']);
 Route::get('/viewer',[App\Http\Controllers\finderController::class,'display']);// blade1
 Route::get('/s', [App\Http\Controllers\finderController::class,'template1']);// blade2
+Route::get('/create-table', [createTableController::class, 'createTable']);
+Route::get('/database', [createTableController::class, 'create_Table']);
